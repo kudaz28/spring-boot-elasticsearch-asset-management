577597d93b6082da49303ac72ad856586dadda07
William Zhou <williamkzhou@hotmail.com>
2018-04-19 12:02:53 +0100

    Initial commit of Spring Initialised project

---
+++ src/main/java/org/masterwill/asset/manager/springbootelasticsearchassetmanagement/SpringBootElasticsearchAssetManagementApplication.java
@@ -0,0 +1,12 @@
+package org.masterwill.asset.manager.springbootelasticsearchassetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticsearchAssetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElasticsearchAssetManagementApplication.class, args);
+	}
+}
---
+++ src/test/java/org/masterwill/asset/manager/springbootelasticsearchassetmanagement/SpringBootElasticsearchAssetManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package org.masterwill.asset.manager.springbootelasticsearchassetmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootElasticsearchAssetManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
